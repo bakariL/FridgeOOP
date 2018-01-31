985b32c3d834109582cdb64abca4353ea6cfa4b1
BakariL <bakarilewis@gmail.com>
2018-01-31 14:06:26 -0500

    first commit

---
+++ FridgeOOP/Fridge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeOOP
+{
+    public class Fridge
+    {
+        //fields states
+        private bool IsClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+        //Properties {get set}
+        public bool IsClean
+        {
+            get { return.this.isClean; }
+            set { this.isClean = value; }
+        }
+
+        public string DispenserType
+        {
+            get { return.this.dispenserType; }
+        }
+
+        //Constructors
+
+        //Methods()
+
+    }
+}
