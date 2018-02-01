ae984ef4709fd9be56cf998060f0db52697e19cb
BakariL <bakarilewis@gmail.com>
2018-02-01 09:58:20 -0500

    Created user menu

--- FridgeOOP/Fridge.cs
+++ FridgeOOP/Fridge.cs
@@ -9,7 +9,7 @@ namespace FridgeOOP
     public class Fridge
     {
         //fields states
-        private bool IsClean;
+        private bool isClean;
         private string dispenserType;
         private int foodAmount;
         private bool bulbsWork;
@@ -17,18 +17,109 @@ namespace FridgeOOP
         //Properties {get set}
         public bool IsClean
         {
-            get { return.this.isClean; }
+            get { return this.isClean; }
             set { this.isClean = value; }
         }
 
         public string DispenserType
         {
-            get { return.this.dispenserType; }
+            get { return this.dispenserType; } //use get only when you want to see something but you do not want to be able to change it. 
         }
 
-        //Constructors
+        public int FoodAmount
+        {
+            get { return this.foodAmount; }
+            set { this.foodAmount = value; }
+        }
+
+        public bool BulbsWork
+        {
+            get { return this.bulbsWork; }
+            set { this.bulbsWork = value; }
+        }
+
+        //Constructors are always puublic 
+        public Fridge()
+        {
+            this.dispenserType = "basic water dispenser"; //setting a value to dispener type can be don here or in the method as a set
+        }
+
+        public Fridge(string dispenserType)
+        {
+            this.dispenserType = dispenserType;
+        }
+
+        public Fridge(bool isClean, string dispenserType, int foodAmount, bool bulbsWork)
+        {
+            this.isClean = isClean;
+            this.dispenserType = dispenserType;
+            this.foodAmount = foodAmount;
+            this.bulbsWork = bulbsWork;
+        }
 
         //Methods()
+        //I need a way to dispense water
 
+        public void DispenseWater()
+        {
+            if (dispenserType == "basic water dispenser")
+            {
+                Console.WriteLine("Here's some luckwarm water!");
+            }
+            else if (dispenserType == "deluxe dispenser")
+            {
+                Console.WriteLine("Enjoy your perfect ice water.");
+            }
+            else
+            {
+                Console.WriteLine("What's gonna come out? It's a surpise!");
+            }
+        }
+
+        public string ChangeBubls()
+        {
+            if (bulbsWork == false)
+            {
+                bulbsWork = true;
+                return "The bulbs have been changed.";
+            }
+            else
+            {
+                return "The bulbs don't have to be changed.";
+            }
+        }
+        public void RemoveFood(int foodRemoved)
+        {
+            foodAmount -= foodAmount;
+        }
+
+        public string CheckSupplies()
+        {
+            if (foodAmount >= 80)
+            {
+                return "fully stocked";
+            }
+            else if (foodAmount > 40)
+            {
+                return "getting low";
+            }
+            else
+            {
+                return "time to grocery shop";
+            }
+        } public string Clean()
+        {
+            if (isClean)
+            {
+                isClean = true;
+                return "I cleaned the fridge";
+            }
+            else
+            {
+                return "The fridge is already clean.";            }
+        }
+    
+
+        }
     }
-}
+
--- FridgeOOP/Program.cs
+++ FridgeOOP/Program.cs
@@ -10,6 +10,19 @@ namespace FridgeOOP
     {
         static void Main(string[] args)
         {
+            //Create an interactive console window
+            //Prompt user to choose what to with the fridge
+            //Call mehods on Fride object to do what the user wants
+
+            Console.WriteLine( "Hello, welcome to the fridge app." );
+            Console.WriteLine("What would you like to do with your fridge?");
+            Console.WriteLine("To dispense water, type 1");
+            Console.WriteLine("To change the lightbulb, type 2");
+            Console.WriteLine("TO clean, type 3");
+            Console.WriteLine("To take food, type 4.");
+            Console.WriteLine("To check food supplies, type 5");
+            Console.WriteLine("To see all fridge details, type 6");
+            Console.WriteLine("To quit type 7");
         }
     }
 }
